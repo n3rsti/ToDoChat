9b9e760371f2b532d000686b3e74bbafd26dc33c
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-03-02 16:06:40 +0100

    fix design in: server_detail (invite button) app_base (side nav) server_base (side nav) create_server (create button)

--- todochat/app/templatetags/templatetags.py
+++ todochat/app/templatetags/templatetags.py
@@ -1,9 +1,9 @@
 from django import template
-from users.models import Profile
-from itertools import chain
 from users.models import User, UserInvitation
+from app.models import Server
 
 register = template.Library()
+
 
 @register.filter
 def get_first_num(value, arg):
@@ -14,6 +14,7 @@ def get_first_num(value, arg):
 def shorter_name(value, length):
     return f'{value[:length]}...' if len(value) > length else value
 
+
 @register.filter
 def get_invitation_status(user, me):
     if UserInvitation.objects.filter(invited=user, inviting=me).first() is not None:
@@ -22,4 +23,10 @@ def get_invitation_status(user, me):
         return "invited" # I got invited
     else:
         return "no_invitation"
+
+
+# Returns count of user's tasks in specific server
+@register.filter
+def get_user_task_count(user, server_id):
+    return Server.objects.get(id=server_id).server_tasks.filter(assigned_for=user).count();
 
