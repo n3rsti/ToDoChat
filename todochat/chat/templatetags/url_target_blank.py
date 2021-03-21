686db51e793a40139be251d5dcd5267a5e11cc0c
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-03-21 13:29:41 +0100

    remove old author filter used in chat_room.html

--- todochat/chat/templatetags/url_target_blank.py
+++ todochat/chat/templatetags/url_target_blank.py
@@ -21,22 +21,6 @@ def url_target_blank(text):
 url_target_blank = register.filter(url_target_blank, is_safe=True)
 
 
-@register.filter
-def author_filter(id, index):
-    if index == 1:
-        return False
-    channel = Channel.objects.get(id=id)
-    messages = ChannelMessage.objects.filter(channel=channel).order_by('created')
-    if messages.count() > 50:
-        messages = messages[messages.count() - 50:messages.count()]
-    message = messages[index - 1]
-    previous = messages[index - 2]
-    if message.author == previous.author:
-        return True
-    else:
-        return False
-
-
 @register.filter()
 def chat_author_filter(messages):
     is_same_author = [False]  # First messages should be always displayed with image
