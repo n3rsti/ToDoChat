83910d3cf3d69c37e6034ed816c890cd7b3a8ea0
Krzysztof Witucki <krzysiek.witucki55@gmail.com>
2021-04-08 14:36:26 +0200

    replace OriginValidator with AllowedHostsOriginValidator, because it works the same as previous validator with arguments

--- todochat/todochat/routing.py
+++ todochat/todochat/routing.py
@@ -1,18 +1,16 @@
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import OriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
-from todochat.settings import config
 
 
 application = ProtocolTypeRouter({
 
-    "websocket": OriginValidator(
+    "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
                 chat.routing.websocket_urlpatterns
             )
         ),
-        config["ALLOWED_HOSTS"],
     ),
 })
